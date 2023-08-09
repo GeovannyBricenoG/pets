1031e94f2dc2d19d55906e02c0b571834b9a6ef5
Geovanny Briceño <gebri106126@gmail.com>
2023-08-08 21:52:10 -0600

    Creando el componente eror404 con su css y el Header con su css de la pagina

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Home from "./pages/Home"
 import Sobre from "./pages/Sobre"
+import Page404 from './pages/Page404';
+import Header from './Componets/Header';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -11,10 +13,12 @@ function App() {
     <>
    <Router>
 
+  <Header/>
    <Routes>
 
   <Route path='/' element={<Home/>} />
-  
+  <Route path='/Sobre' element={<Sobre/>} />
+  <Route path='*' element={<Page404/>} />
    </Routes>
 
    </ Router>
